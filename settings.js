b1aca7d5302feecad0beabd5e8a3780c72c1dacd
Finjohns <kelvinonginjo08@gmail.com>
2025-02-28 19:19:50 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,26 +8,26 @@ const chalk = require('chalk')
 if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 
-global.SESSION_ID = process.env.SESSION_ID || 'Bellah~2z40RA7T#VRneuCjwFxSYHupVJSJHZDNmNDE_THLCNxjVd9jGWuw' 
+global.SESSION_ID = process.env.SESSION_ID || 'Bellah~2mIEgSLb#56DxU9D3BFrTBfKH-nep2vUwAAgHuN0QxfOLYW3FZM8' 
 //owmner v card
-global.ytname = "YT: GIDDYTENNOR" //ur yt chanel name
-global.socialm = "IG: @GIDDYTENNOR" //ur github or insta name
+global.ytname = "YT: JINWIILONGINJO" //ur yt chanel name
+global.socialm = "IG: @JINWIILONGINJO" //ur github or insta name
 global.location = "Kenya" //ur location
 
 //new
-global.botname = process.env.BOT_NAME ||'𝐁𝐞𝐥𝐥𝐚𝐡 𝐗𝐦𝐝' //enter your  bot name here
-global.ownernumber = process.env.OWNER_NUMBER ||'254104245659' //ur owner number
-global.ownername = '© TENNOMODZ' //ur owner name
-global.websitex = "https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
-global.wagc = "https://chat.whatsapp.com/CzFlFQrkdzxFw0pxCBYM7H" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
+global.botname = process.env.BOT_NAME ||'𝗕𝗲𝗹𝗹𝗿𝗼𝗺 𝐗𝐦𝐝' //enter your  bot name here
+global.ownernumber = process.env.OWNER_NUMBER ||'254769365617' //ur owner number
+global.ownername = '© JINWIIL' //ur owner name
+global.websitex = "https://whatsapp.com/channel/0029VaxZbeSDTkJwBgUb9u3N" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
+global.wagc = "https://chat.whatsapp.com/EqDVC9BGF9k86ZMHdjxY0d" //"https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES"
 global.themeemoji = '🪀'
-global.wm = "GIDDY TENNOR"
-global.botscript = 'https://whatsapp.com/channel/0029VaPZWbY1iUxVVRIIOm0D' //'https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES' //script link
-global.packname = process.env.PACK_NAME ||"BELLAH XMD" //enter your stickers author name here
-global.author = "Giddy-Tennor"
-global.creator = "254703726139@s.whatsapp.net"
+global.wm = "JINWIIL ONGINJO"
+global.botscript = 'https://whatsapp.com/channel/0029VaxZbeSDTkJwBgUb9u3N' //'https://chat.whatsapp.com/JmsgzJllAAB8zHfQcJXxES' //script link
+global.packname = process.env.PACK_NAME ||"LOREIN XMD" //enter your stickers author name here
+global.author = "Jinwiil Onginjo"
+global.creator = "254769365617@s.whatsapp.net"
 global.xprefix = process.env.BOT_PREFIX ||'.'
-global.premium = ["254703726139"] // Premium User
+global.premium = ["254784320958"] // Premium User
 global.hituet = 0
 
 //bot sett
@@ -47,7 +47,7 @@ global.autorecordtype = process.env.AUTO_RECORDTYPE || 'false'
 global.mess = {
 	limit: 'Your limit is up <\>',
 	nsfw: 'Nsfw is disabled in this group, Please tell the admin to enable',
-	owner: 'Bellah Xmd owner only<\>',
+	owner: 'Lorein Xmd owner or Jinwiil only<\>',
     admin: 'Bot is not admin<\>',
     group: 'feature for group only<\>',
     done: 'Done ✓',
@@ -64,3 +64,4 @@ fs.watchFile(file, () => {
     delete require.cache[file]
     require(file)
 })
+	
